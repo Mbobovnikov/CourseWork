a04753517976406cc0798c6fe3a1019d63de7d86
mbobovnikov <mbobovnikov@gmail.com>
2019-10-20 21:49:40 +0300

    Добавлены методы к классу Resistend

--- CourseWork/Resident.cs
+++ CourseWork/Resident.cs
@@ -8,15 +8,42 @@ using System.Runtime.CompilerServices;
 
 namespace CourseWork
 {
-   public class Resident : INotifyPropertyChanged
+    public class Resident //: INotifyPropertyChanged
     {
         private string FirstName;
         private string SecondName;
-        private string PatronymicResident; 
+        private string PatronymicResident;
         private string InformationAboutResident;
         private int AgeResident;
         private int RoomNumber;
         private bool SexResident;
-       
+
+        public string Firstname
+        {
+            get { return FirstName; }
+            set { FirstName = value;
+            }
+        }
+        public string Secondname
+        {
+            get { return SecondName; }
+            set { SecondName = value; }
+        }
+        public string Patronymicresident
+        {
+            get { return PatronymicResident; }
+            set { PatronymicResident = value; }
+        }
+        public string Infomation
+        {
+            get { return InformationAboutResident; }
+            set { InformationAboutResident= value; }
+        }
+         public int Ageresident
+        {
+            get { return AgeResident; }
+            set { AgeResident = value; }
+        }
+
     }
 }
